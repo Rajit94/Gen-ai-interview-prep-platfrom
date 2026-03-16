7f1fb9c0fad4dc472bdbce0ad0ab0a00caabda53
Rajit94 <baghelrajit94@gmail.com>
2026-03-16 14:27:17 +0530

    created UI of Login & register

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
