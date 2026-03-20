732611e7d7be93cfbbb88a5e48444b56450fb06e
Rajit94 <baghelrajit94@gmail.com>
2026-03-20 18:13:32 +0530

    API setup of login,register,logout

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../auth.form.scss"
 import { Link, useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 
 const Login = () => {
+    
+    const { loading, handleLogin } = useAuth()
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        handleLogin({email, password})
 }
 
+ if(loading){
+    return (<main><h1>Loading.......</h1></main>)
+ }
     return (
 
         <main>
@@ -20,11 +29,15 @@ const Login = () => {
 
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter email address"></input>
+                        <input 
+                        onChange={(e)=>{ setEmail(e.target.value) }}
+                        type="email" id="email" name="email" placeholder="Enter email address"></input>
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter password"></input>
+                        <input 
+                        onChange={(e)=>{ setPassword(e.target.value) }}
+                        type="password" id="password" name="password" placeholder="Enter password"></input>
                     </div>
 
                     <button className="button primary-button">Login</button>
