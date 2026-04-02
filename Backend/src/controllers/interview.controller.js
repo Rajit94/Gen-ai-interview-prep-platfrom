b6b61f307a2b5c1b13cdde019022fce3a2b6782e
Rajit94 <baghelrajit94@gmail.com>
2026-04-02 14:31:13 +0530

    successfully integrated Ai api's in backend

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -3,19 +3,29 @@ const generateInterviewReport = require("../services/ai.service")
 const interviewReportModel = require("../models/interviewReport.model")
 
 async function generateInterviewReportController(req, res){
-const resumeFile = req.file
 
-const resumeContent = pdfParse(req.file.buffer)
+
+const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+
 const { selfDescription, jobDescription } = req.body
 
 const interviewreportByAi = await generateInterviewReport({
-    resume: resumeContent,
+    resume: resumeContent.text,
     selfDescription,
     jobDescription
 })
 
 const interviewReport = await interviewReportModel.create({
-    user: req.user.id
+    user: req.user.id,
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+    ...interviewreportByAi
+})
+
+res.status(201).json({
+    message: "Interview report generated successfully",
+    interviewReport
 })
 
 }
---
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='home'>
+        <div className="left">
+            <textarea name='jobDescription' id='jobDescription'></textarea>
+        </div>
+    </main>
+  )
+}
+
+export default Home
