fba9cad9c3328d796f29227ffd75064da4f1500a
Rajit94 <baghelrajit94@gmail.com>
2026-04-17 14:53:48 +0530

    Completed final UI changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
 import { InterviewProvider } from "./features/interview/interview.context.jsx"
-function App() {
-  
-  return (
 
+function App() {
+
+  return (
     <AuthProvider>
       <InterviewProvider>
-    <RouterProvider router={router} />
-    </InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
     </AuthProvider>
   )
 }
